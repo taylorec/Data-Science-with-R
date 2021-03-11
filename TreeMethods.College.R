3803d8036b4acef89c1c985df713f640b94d5996
taylorec <66334307+taylorec@users.noreply.github.com>
2021-03-11 06:30:12 -0600

    Add files via upload

---
+++ TreeMethods.College.R
@@ -0,0 +1,102 @@
+# This project classifies schools as Private or Public based off their features.
+
+library(ISLR)
+?College
+head(College)
+
+df<-College
+
+# Exploratory Data Analysis
+library(ggplot2)
+
+# Scatterplot of Grad.Rate versus Room.Board, colored by the Private column
+ggplot(df,aes(Room.Board,Grad.Rate)) + geom_point(aes(color=Private))
+
+# Histogram of full time undergrad students, color by Private
+ggplot(df,aes(F.Undergrad)) + geom_histogram(aes(fill=Private),color='black',bins=50)
+
+# Histogram of Grad.Rate colored by Private
+ggplot(df,aes(Grad.Rate)) + geom_histogram(aes(fill=Private),
+	color='black',bins=50) # a datapoint is above 100%
+
+#Clean data
+# What college had a Graduation Rate of above 100%?
+subset(df,Grad.Rate > 100)
+
+df['Cazenovia College','Grad.Rate'] <- 100 # Change it 100%
+
+# Train Test Split
+library(caTools)
+set.seed(101) 
+sample = sample.split(df$Private, SplitRatio = .70)
+train = subset(df, sample == TRUE)
+test = subset(df, sample == FALSE)
+
+#Decision Tree
+library(rpart)
+tree <- rpart(Private ~.,method='class',data = train)
+tree.preds <- predict(tree,test)
+head(tree.preds) # Check the Head of the predicted values.
+
+# Create Yes/No Label for a Private column
+tree.preds <- as.data.frame(tree.preds)
+joiner <- function(x){
+    if (x>=0.5){
+        return('Yes')
+    }else{
+        return("No")
+    }
+}
+
+tree.preds$Private <- sapply(tree.preds$Yes,joiner)
+head(tree.preds)
+tree.CM <- table(tree.preds$Private,test$Private) # confusion matrix
+
+library(rpart.plot)
+prp(tree) # tree plot
+
+
+# Random Forest
+library(randomForest)
+rf.model <- randomForest(Private ~ . , data = train,importance = TRUE)
+rf.model$confusion
+rf.model$importance
+fr.preds <- predict(rf.model,test)
+rf.CM <- table(fr.preds,test$Private)
+
+#Accuracy
+(tree.CM[1]+tree.CM[4])/sum(tree.CM) # Tree Accuracy
+(rf.CM[1]+rf.CM[4])/sum(rf.CM) # Random Forest Accuracy
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
